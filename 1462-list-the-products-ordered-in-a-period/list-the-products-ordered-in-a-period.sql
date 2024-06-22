2f2b5565f6b392245f66adbb2272be582f733e46
OllieClarke <43183262+OllieClarke@users.noreply.github.com>
2024-06-22 19:26:06 +0100

    Time: 355 ms (95.58%) | Memory: 0B (100.00%) - LeetSync

--- 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -2,11 +2,10 @@
 --get products with over 100 units in feb
 WITH agg as(
     SELECT product_id
-    , left(order_date,7) as month
     , SUM(unit) as unit
     FROM Orders
     WHERE left(order_date,7) = '2020-02'
-    GROUP BY product_id, left(order_date,7)
+    GROUP BY product_id
     HAVING sum(unit)>=100
 )
 SELECT product_name
