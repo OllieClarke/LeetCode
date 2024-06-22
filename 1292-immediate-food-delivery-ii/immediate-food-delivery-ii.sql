038ea7255ab764788a2450c23116f5cf6a945031
OllieClarke <43183262+OllieClarke@users.noreply.github.com>
2024-06-22 14:47:53 +0100

    Update immediate-food-delivery-ii.sql

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,12 +1,16 @@
 /* Write your T-SQL query statement below */
+--attach a row number based on customer order date
 WITH rw as(
     SELECT ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date ASC) as "row"
     , *
     FROM delivery
-), mm as (
+)
+--just keep the first order per customer    
+    , mm as (
     SELECT *
     from rw
     WHERE row=1
 )
+--find percentage immediate    
 Select ROUND((SUM(CASE when customer_pref_delivery_date = order_date then 1 else 0 END)*1.00)/SUM(1.00)*100,2) as immediate_percentage
 from mm
