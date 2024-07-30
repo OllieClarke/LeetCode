430c03533251416aa36fc87542a082bb26a8820a
OllieClarke <43183262+OllieClarke@users.noreply.github.com>
2024-07-30 10:28:30 +0100

    Time: 1414 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/employees-whose-manager-left-the-company.sql
@@ -1,15 +1,10 @@
 /* Write your T-SQL query statement below */
-/*Find the managers who left*/
-With lft as(
-    select a.manager_id
+ select a.employee_id
     from Employees a
     LEFT JOIN Employees b
     on a.manager_id = b.employee_id
-    WHERE b.employee_id IS NULL AND a.manager_id IS NOT NULL
-)
-/*Get the employees with a left manager and salary below 30k*/
-SELECT a.employee_id
-FROM Employees a
-JOIN lft on a.manager_id = lft.manager_id
-WHERE salary <30000
-GROUP BY a.employee_id
+    where b.employee_id IS NULL
+    AND a.manager_id IS NOT NULL
+    AND a.salary <30000
+    GROUP BY a.employee_id
+    ORDER BY a.employee_id
